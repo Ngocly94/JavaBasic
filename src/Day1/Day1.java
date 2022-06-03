5191ae925b9feca644f7cb5a494274c42f568af9
Ngọcc <ngocc@MacBook-Air-cua-Ngocc.local>
2022-06-03 23:15:10 +0700

    Change day 1and main class

---
+++ src/Day1/Day1.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class Day1 {
+//Ex1
+	public static void printString(String x) {
+		System.out.println(x);
+	}
+
+//Ex2
+public static void printString1(String a,String b,String c,String d) {
+System.out.println(a+ "\n"+ b + "\n" + c + "\n" + d);
+}
+
+//Ex3
+public static void printstring2(int a,double d,double c) {
+	System.out.println(a+ "\n"+ d + "\n" + c );
+	
+}
+}
---
+++ src/Day1/Main.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Day1.printString("Hello world");
+		Day1.printString1("++++++", "@@@@@@", "******", "######");
+		Day1.printstring2(10, 20.3, 3.14785);
+	}
+
+}
